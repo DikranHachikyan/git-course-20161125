d38adbba0a24ac9cf1ab5887e0e9c99ef321c2d9
Dikran Hachikyan <dhachikian@expert-bg.org>
2016-11-25 12:51:15 +0200

    Add walk directory method

--- FileSearchApp.java
+++ FileSearchApp.java
@@ -7,7 +7,20 @@ public class FileSearchApp {
     
     public static void main(String[] args) {
         FileSearchApp app = new FileSearchApp();
-    
+    	switch( Math.min( args.length , 3)){
+            case 0:
+                System.out.println("USAGE: FileSearchApp path [regex] [zipfile]");
+                return;
+            case 3: app.setZipFileName(args[2]);
+            case 2: app.setRegex(args[1]);
+            case 1: app.setPath(args[0]);
+        }
+        try{
+            app.walkDirectory(app.getPath());
+        }
+        catch( Exception e){
+            e.printStackTrace();
+        }
     }
 
     public String getPath() {
@@ -33,5 +46,10 @@ public class FileSearchApp {
 
     public void setZipFileName(String zipFileName) {
         this.zipFileName = zipFileName;
+    }
+
+    public void walkDirectory( String path) throws IOException {
+    //    walkDirectoryJava8(path);
+    //    zipFilesJava7();
     }
 }
